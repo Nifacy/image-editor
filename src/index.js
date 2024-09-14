5b54f697debe34921bbdfac38c12bf1e028d7314
Nifacy <mai.grishin.alexey@gmail.com>
2024-09-14 17:15:16 +0300

    add react & jsx support

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log("Image Editor project is up and running!");
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import Header from './Header';
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+root.render(<Header />);
--- webpack.config.js
+++ webpack.config.js
@@ -11,10 +11,27 @@ module.exports = {
             directory: path.resolve(__dirname, 'public'),
         },
         devMiddleware: {
-            publicPath: '/dist/', // Путь для сервировки бандла
+            publicPath: '/dist'
         },
         hot: true,
         open: false,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
     },
     mode: 'development',
 };
